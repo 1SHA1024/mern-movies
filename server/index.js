e438cd6688885efcbd63e1b2f1b3c4c091f2a270
Israel Peled <israelpeled@gmail.com>
2021-04-12 14:33:20 +0300

    update

--- server/index.js
+++ server/index.js
@@ -10,26 +10,14 @@ const db = require('./DB');
 //import router module
 const moviesRouter = require('./routes/movie-router')
 //configuration of the port based on the env
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 //use of body-parser in order to reach req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded())
 //use of cors so our server will be able to get requests
 app.use(cors())
 
-db.on('error',()=>{console.log("connection error")})
-
-app.listen(PORT,()=>{
-    console.log(`mern server is live and up on port: ${PORT}`);
-})
-app.get('/',(req,res)=>{
-    res.send("get api is working fine")
-})
-
-app.use('/movies',moviesRouter)
-
-
-
+//*******************************************************************
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
@@ -40,3 +28,16 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
 }
+//*******************************************************************
+
+db.on('error',()=>{console.log("connection error")})
+
+app.listen(PORT,()=>{
+    console.log(`mern server is live and up on port: ${PORT}`);
+})
+
+// app.get('/',(req,res)=>{
+//     res.send("get api is working fine")
+// })
+
+app.use('/movies',moviesRouter)
